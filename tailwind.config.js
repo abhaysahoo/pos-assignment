0aef6873c2e800df1a1de40bed50ac365b1e5f1e
Abhay Sahoo <abhayshaw19999@gmail.com>
2024-09-13 10:54:46 +0530

    This is my start time

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,103 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
-  },
-  plugins: [],
-};
+	darkMode: ["class"],
+	content: [
+		'./pages/**/*.{js,jsx}',
+		'./components/**/*.{js,jsx}',
+		'./app/**/*.{js,jsx}',
+		'./src/**/*.{js,jsx}',
+		'./lib/**/*.{js,jsx}',
+	],
+	prefix: "",
+	theme: {
+		container: {
+			center: true,
+			padding: "2rem",
+			screens: {
+				"xl": "1280px",
+				"2xl": "1400px",
+			},
+		},
+		extend: {
+			colors: {
+				border: "var(--border)",
+				input: "var(--input)",
+				ring: "var(--ring)",
+
+				background: "var(--background)",
+				foreground: "var(--foreground)",
+
+				disabled: "var(--disabled)",
+				"disabled-foreground": "var(--disabled-foreground)",
+
+				"brand": {
+					"50": "var(--brand-50)",
+					"100": "var(--brand-100)",
+					"200": "var(--brand-200)",
+					"300": "var(--brand-300)",
+					"400": "var(--brand-400)",
+					"500": "var(--brand-500)",
+					"600": "var(--brand-600)",
+					"700": "var(--brand-700)",
+					"800": "var(--brand-800)",
+					"900": "var(--brand-900)",
+					"950": "var(--brand-950)",
+				},
+
+				primary: {
+					DEFAULT: "var(--primary)",
+					foreground: "var(--primary-foreground)",
+					hover: "var(--primary-hover)",
+					loading: "var(--primary-loading)",
+				},
+				secondary: {
+					DEFAULT: "var(--secondary)",
+					foreground: "var(--secondary-foreground)",
+					hover: "var(--secondary-hover)",
+					loading: "var(--secondary-loading)",
+				},
+				destructive: {
+					DEFAULT: "var(--destructive)",
+					foreground: "var(--destructive-foreground)",
+					hover: "var(--destructive-hover)",
+					loading: "var(--destructive-loading)",
+				},
+				muted: {
+					DEFAULT: "var(--muted)",
+					foreground: "var(--muted-foreground)",
+				},
+				accent: {
+					DEFAULT: "var(--accent)",
+					foreground: "var(--accent-foreground)",
+				},
+				popover: {
+					DEFAULT: "var(--popover)",
+					foreground: "var(--popover-foreground)",
+				},
+				card: {
+					DEFAULT: "var(--card)",
+					foreground: "var(--card-foreground)",
+				},
+			},
+
+			fontFamily: {
+			},
+
+			keyframes: {
+				"accordion-down": {
+					from: { height: "0" },
+					to: { height: "var(--radix-accordion-content-height)" },
+				},
+				"accordion-up": {
+					from: { height: "var(--radix-accordion-content-height)" },
+					to: { height: "0" },
+				},
+			},
+			animation: {
+				"accordion-down": "accordion-down 0.2s ease-out",
+				"accordion-up": "accordion-up 0.2s ease-out",
+			},
+		},
+	},
+	plugins: [require("tailwindcss-animate")],
+}
